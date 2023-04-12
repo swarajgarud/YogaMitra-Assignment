a48c95f6f7a24f488db94d948b090c04df7e6f0e
swarajgarud <95527767+swarajgarud@users.noreply.github.com>
2023-04-12 19:46:34 +0530

    Update Assignment 1.js

--- Assignment 1.js
+++ Assignment 1.js
@@ -1,11 +1,11 @@
 //Write a function in JavaScript that takes an array of integers and returns the sum of all even numbers in the array.
 //Ans:-  
 function sumOfEvenNumbers(arr) {
-              let sum = 0;
-              for (let i = 0; i < arr.length; i++) {
-                if (arr[i] % 2 === 0) {
-                  sum += arr[i];
-               }
-            }
-         return sum;
-       }
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+   if (arr[i] % 2 === 0) {
+    sum += arr[i];
+   }
+  }
+  return sum;
+ }
